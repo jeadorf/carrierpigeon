2175624683d32c0de99dcac696c177cc9385f896
Marek Kubica <marek@xivilization.net>
2009-05-28 15:21:20 +0200

    Symbolic macros instead of bitfiddling

--- experiments/buttons/main.c
+++ experiments/buttons/main.c
@@ -4,39 +4,48 @@
 #define PORTC_MASK 0x10
 #define WAIT_TIME 250.0
 
+#define SET_BIT(port, bit)   ((port) |= (1 << (bit)))
+#define CLEAR_BIT(port, bit) ((port) &= ~(1 << (bit)))
+
 void blink(void);
+int get_key(void);
+
+int get_key(void) 
+{
+	int keyvalue = 0;
+	
+	return keyvalue;
+}
 
 int main(void)
 {
-  int col2;
-  int row1;
-
-  DDRC = 0x1F;
-  DDRD = 0x00;
-
-  while (1) {
-    PORTC |= 0x0E;
-    if (!bit_is_set(PIND, 4)) {
-      blink();
-      PORTD |= 0x10;
-    }
-    if (!bit_is_set(PIND, 5)) {
-      blink();
-      PORTD |= 0x20;
-    }
-    if (!bit_is_set(PIND, 6)) {
-      blink();
-      PORTD |= 0x40;
-    }
-  }
-
-  return 0;
+	int key;
+	
+	while (1) {
+		key = get_key();
+		if (key == 4)
+			blink();
+	}
+	
+	return 0;
 }
+
+void led_on(void)
+{
+	SET_BIT(DDRC, DDC4);
+	SET_BIT(PORTC, PC4);
+}
+
+void led_off(void)
+{
+	CLEAR_BIT(PORTC, PC4);
+}
+
 
 void blink(void)
 {
-  PORTC |= LED_GREEN;
-  _delay_ms(WAIT_TIME);
-  PORTC &= ~LED_GREEN;
-  _delay_ms(WAIT_TIME);
+	led_on();
+	_delay_ms(WAIT_TIME);
+	led_off();
+	_delay_ms(WAIT_TIME);
 }  
