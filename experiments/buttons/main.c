4b62f4a0656e58721d2a65258ceea58cc34ca953
Marek Kubica <marek@xivilization.net>
2009-05-28 17:37:43 +0200

    Commented code, removed duplication

--- experiments/buttons/main.c
+++ experiments/buttons/main.c
@@ -8,43 +8,42 @@
 #define CLEAR_BIT(port, bit) ((port) &= ~(1 << (bit)))
 
 void blink(void);
-int get_key(void);
+char get_key(void);
 void blink_times(int n);
 
-int get_key(void) 
+char get_key(void)
 {
 	// result: default 0 (no key pressed)
-	int keyvalue = 0;
+	char col, othercol, row, keyvalue = 0;
 
 	// set DDRC3-0 to WRITE
 	DDRC |= 0b00001111;
 
-	int col, othercol;
+	// iterate over all 4 COLs
 	for (col = PC3; col >= PC0; col--)
 	{
+		// set all cols to 1 except for the current col
 		for (othercol = PC3; othercol >= PC0; othercol--) {
 			if (othercol != col)
 				SET_BIT(PORTC, othercol);
 		}
+		// set the current col to 0
 		CLEAR_BIT(PORTC, col);
 		// set DDRD6-4 to READ (needs to be in a loop, since DDR is strange)
 		DDRD &= 0b10001111;
-		
-		//int row;
-		//for (row = PD6; row >= PD4; row--) {
+
+		// loop over all rows (from PD6 - PD4)
+		for (row = PD6; row >= PD4; row--) {
+			// if the bit is clear, the button is pressed
+			if (bit_is_clear(PIND, row)) {
+				// unpress it
+				SET_BIT(PORTD, row);
 			
-		if (bit_is_clear(PIND, PIND6)) {
-			SET_BIT(PORTD, PD6);
-			
-			return (col * 3) + 3;
-		}
-		if (bit_is_clear(PIND, PIND5)) {
-			SET_BIT(PORTD, PD5);
-			return (col * 3) + 2;
-		}
-		if (bit_is_clear(PIND, PIND4)) {
-			SET_BIT(PORTD, PD4);
-			return (col * 3) + 1;
+				// return the value of the key
+				// col * 3 because each col has 3 keys
+				// row - 3 because the PDs have offset 3
+				return (col * 3) + (row - 3);
+			}
 		}
 	}
 
@@ -53,7 +52,7 @@ int get_key(void)
 
 int main(void)
 {
-	int key;
+	char key;
 	
 	while (1) {
 		key = get_key();
