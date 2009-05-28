819cce8ba475063108a9a19174c094beac74a7fe
Marek Kubica <marek@xivilization.net>
2009-05-28 17:57:10 +0200

    Put LED control into its own files

--- experiments/buttons/main.c
+++ experiments/buttons/main.c
@@ -1,52 +1,35 @@
+#include <stdbool.h>
 #include "main.h"
 #include "buttons.h"
+#include "led.h"
 
-#define LED_GREEN 0x10
-#define PORTC_MASK 0x10
-#define WAIT_TIME 150.0
-
-void blink(void);
-void led_on(void);
-void led_off(void);
-void blink_times(int n);
-
-
-int main(void)
+// rename to main to get this version
+int main_hold(void)
 {
 	char key;
 	
-	while (1) {
+	while (true)
+	{
 		key = get_key();
-		blink_times(key);
+		if (key != 0)
+			led_on();
+		else
+			led_off();
 	}
 	
 	return 0;
 }
 
-void blink_times(int n) 
+// rename to something else to deactivate
+int main(void)
 {
-	int i;
-	for (i = 0; i < n; i++) {
-		blink();
+	char key;
+	
+	while (true)
+	{
+		key = get_key();
+		blink_times(key);
 	}
-}
-
-void led_on(void)
-{
-	SET_BIT(DDRC, DDC4);
-	SET_BIT(PORTC, PC4);
-}
-
-void led_off(void)
-{
-	CLEAR_BIT(PORTC, PC4);
-}
-
-
-void blink(void)
-{
-	led_on();
-	_delay_ms(WAIT_TIME);
-	led_off();
-	_delay_ms(WAIT_TIME);
+	
+	return 0;
 }
