186e222ec250e993b9f97ac13b856a710e0e4adb
Julius Adorf <jeadorf@gmail.com>
2009-06-21 17:33:25 +0200

    Added docs: bit matrix representation of characters

--- experiments/lcd/text.c
+++ experiments/lcd/text.c
@@ -6,8 +6,54 @@
 
 void lcd_draw_char(unsigned char c)
 {
-    // insert a little space
+    // Insert a little space
     lcd_write(0x0);
+    
+    // Characters are represented as bit matrices. There are no lower-case
+    // characters.
+    //
+    // Width: 4-bit, usually, but don't rely on this!
+    // Height: 5-bit
+    // Padding: top 1-bit, bottom 1-bit (by convention)
+    // 
+    // In order to convert the hex representation of a character back to a
+    // readable matrix representation, write the hex values as column vectors
+    // in binary representation. Example:
+    //
+    // F: 
+    //  lcd_write(0x7c);
+    //  lcd_write(0x50);
+    //  lcd_write(0x50);
+    //  lcd_write(0x40);
+    //
+    //      <-->
+    //
+    //  0 0 0 0
+    //  x x x x
+    //  7 5 5 4
+    //  c 0 0 0
+    //
+    //      <-->
+    //
+    //  0 0 0 0
+    //  1 1 1 1
+    //  1 0 0 0
+    //  1 1 1 1
+    //  1 0 0 0
+    //  1 0 0 0
+    //  0 0 0 0
+    //  0 0 0 0
+    //
+    //      <-->
+    //
+    //         
+    //  1 1 1 1
+    //  1      
+    //  1 1 1 1
+    //  1      
+    //  1      
+    //         
+    //         
     
     switch (c) 
     {
