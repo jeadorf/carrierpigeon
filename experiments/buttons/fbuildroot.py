0ed7cc9a916529085bb0cb401c0cb73522af601e
Marek Kubica <marek@xivilization.net>
2009-07-06 15:18:38 +0200

    Fbuild can now build without warnings

--- experiments/buttons/fbuildroot.py
+++ experiments/buttons/fbuildroot.py
@@ -4,16 +4,20 @@ import fbuild.builders.c
 PROG_METHOD = 'avrisp2'
 PORT = 'usb'
 MCU = 'atmega8515'
+CPU_HZ = 16 * 10**6
 
 def build():
-    static = fbuild.builders.c.guess_static(platform={'avr', 'gcc'}, mmcu=MCU)
+    static = fbuild.builders.c.guess_static(
+            platform={'avr', 'gcc'},
+            mmcu=MCU,
+            optimize=['-O2'],
+            macros={'F_CPU': CPU_HZ})
     exe = static.build_exe('main', ['main.c', 'buttons.c', 'led.c'])
 
     fbuild.execute(['avr-strip', exe])
     fbuild.execute(['avr-objcopy', '-O', 'srec', exe, 'main.srec'])
     fbuild.execute(['avr-objcopy', '-O', 'ihex', exe, 'main.hex'])
     fbuild.execute(['avr-objcopy', '-O', 'binary', exe, 'main.flash'])
-    #fbuild.execute(['avrdude', '-U', 'foo.srec'])
     fbuild.execute(['avrdude', '-c', PROG_METHOD,
         '-P', PORT, '-p', MCU,
         '-U', 'hfuse:w:0xdd:m',
