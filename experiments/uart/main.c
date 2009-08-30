390cd96b992a046d0df59ea7cd30e5b39eb27636
Marek Kubica <marek@xivilization.net>
2009-08-30 23:53:46 +0200

    UART code cleanup

--- experiments/uart/main.c
+++ experiments/uart/main.c
@@ -1,4 +1,20 @@
-#include <stdbool.h>
 #include "main.h"
-#include "led.h"
 
+int main(void) {
+    int c;
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE,11059200UL));
+    sei();
+
+    lcd_init();
+    lcd_clear();
+    lcd_set_page(0);
+    lcd_set_column(10);
+    lcd_draw_char('S');
+
+    while (1) {
+        c = uart_getc();
+        if (!(c & UART_NO_DATA)) {
+            lcd_draw_char(c & 0xFF);
+        }
+    }
+}
