494798534428199f5cd2cedd6a85b344087c4eb8
Julius Adorf <jeadorf@gmail.com>
2009-09-02 21:11:59 +0200

    fixed bluetooth example, wait until inquiry completes

--- mobile/desktop-letterbox-client/src/main/java/carrierpigeon/desktopclient/DesktopLetterboxClient.java
+++ mobile/desktop-letterbox-client/src/main/java/carrierpigeon/desktopclient/DesktopLetterboxClient.java
@@ -1,5 +1,7 @@
 package carrierpigeon.desktopclient;
 
+import java.io.IOException;
+import java.util.Vector;
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.DiscoveryListener;
@@ -8,43 +10,52 @@ import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
 
 /**
- * quick case study
- * 
- * @author Julius Adorf
+ * Derived from the minimal example found at
+ * http://bluecove.org/bluecove/apidocs/index.html
  */
 public class DesktopLetterboxClient {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
-        System.out.println("Usage: DesktopLetterboxClient");
-        DesktopLetterboxClient client = new DesktopLetterboxClient();
-        client.discoverDevices();
-    }
+    public static final Vector discoveredDevices = new Vector();
 
-    public void discoverDevices() throws Exception {
-        LocalDevice ld = LocalDevice.getLocalDevice();
-        final DiscoveryAgent agent = ld.getDiscoveryAgent();
-        final DiscoveryListener listener = new DiscoveryListener() {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-            public void deviceDiscovered(RemoteDevice device, DeviceClass arg1) {
-                System.out.println("deviceDiscovered: " + device.getBluetoothAddress());
+        final Object inquiryCompletedEvent = new Object();
+
+        discoveredDevices.clear();
+
+        DiscoveryListener listener = new DiscoveryListener() {
+
+            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+                discoveredDevices.addElement(btDevice);
+                try {
+                    System.out.println("     name " + btDevice.getFriendlyName(false));
+                } catch (IOException cantGetDeviceName) {
+                }
             }
 
-            public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
-                System.out.println("servicesDiscovered");
+            public void inquiryCompleted(int discType) {
+                System.out.println("Device Inquiry completed!");
+                synchronized(inquiryCompletedEvent){
+                    inquiryCompletedEvent.notifyAll();
+                }
             }
 
-            public void serviceSearchCompleted(int arg0, int arg1) {
-                System.out.println("serviceSearchCompleted");
+            public void serviceSearchCompleted(int transID, int respCode) {
             }
 
-            public void inquiryCompleted(int arg0) {
-                System.out.println("inquiryCompleted");
+            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
             }
         };
-        System.out.println("Starting LIAC inquiry");
-        agent.startInquiry(DiscoveryAgent.LIAC, listener);
+
+        synchronized(inquiryCompletedEvent) {
+            boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+            if (started) {
+                System.out.println("wait for device inquiry to complete...");
+                inquiryCompletedEvent.wait();
+                System.out.println(discoveredDevices.size() +  " device(s) found");
+            }
+        }
     }
+
 }
