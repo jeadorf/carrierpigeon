cc3314a2f6fdd8bd3f15c44287ab6342810c02cd
Marek Kubica <marek@xivilization.net>
2009-09-05 12:29:45 +0200

    Added display clearing functionality

--- experiments/uart/main.c
+++ experiments/uart/main.c
@@ -4,9 +4,10 @@
 #include "main.h"
 #include "uart.h"
 
-int main(void) {
+int main(void)
+{
     int c;
-    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE,11059200UL));
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, 11059200UL));
     sei();
 
     lcd_init();
@@ -15,10 +16,27 @@ int main(void) {
     lcd_set_column(10);
     lcd_draw_char('S');
 
-    while (true) {
+    while (true)
+    {
         c = uart_getc();
-        if (!(c & UART_NO_DATA)) {
+        reset_lcd();
+        if (!(c & UART_NO_DATA))
+        {
             lcd_draw_char(c & 0xFF);
         }
+    }
+}
+
+void reset_lcd(void)
+{
+    static unsigned long numcalls = 0;
+
+    numcalls++;
+    if (numcalls >= 120000) {
+        lcd_clear();
+        lcd_set_page(0);
+        lcd_set_column(10);
+        lcd_draw_char('S');
+        numcalls = 0;
     }
 }
--- experiments/uart/main.h
+++ experiments/uart/main.h
@@ -2,3 +2,5 @@
 #define CLEAR_BIT(port, bit) ((port) &= ~(1 << (bit)))
 
 #define UART_BAUD_RATE 19200
+
+void reset_lcd(void);
