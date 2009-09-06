13dd69dc736a85f96fd8160dec8cebac1cb7099e
Marek Kubica <marek@xivilization.net>
2009-09-06 14:05:16 +0200

    Added half-working read line functionality

--- experiments/uart/main.c
+++ experiments/uart/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdbool.h>
 #include <avr/io.h> 
 #include <avr/interrupt.h>
@@ -8,33 +9,62 @@
 int main(void)
 {
     int c;
+    char* text;
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, 11059200UL));
     sei();
 
     lcd_init();
     lcd_clear();
-    lcd_set_page(0);
-    lcd_set_column(10);
-    lcd_draw_char('S');
+    lcd_set_page(7);
+    lcd_set_column(5);
     // set the prescaler of the timer to 1024
     set_bit(TCCR1B, CS12);
     set_bit(TCCR1B, CS10);
 
+
     while (true)
     {
-        c = uart_getc();
-        reset_lcd();
-        if (!(c & UART_NO_DATA))
+        //TODO: implement 'CONNECT' detection to only echo back relevant
+        // stuff
+        text = uart_readline();
+        if (text != NULL)
         {
-            //TODO: implement 'CONNECT' detection to only echo back relevant
-            // stuff
-
-            // echo it back to the sender
-            //uart_putc(c);
-            // display it on the LCD (zero the higher byte before)
-            lcd_draw_char(c & 0xff);
+            reset_lcd();
+            lcd_display_string(text);
         }
     }
+}
+
+char* uart_readline(void)
+{
+    int c;
+    static char* result = NULL;
+    static size_t size = 1;
+    static unsigned char cr = 0, lf = 0;
+
+    c = uart_getc();
+    if (!(c & UART_NO_DATA))
+    {
+        c = c & 0xFF;
+        if (c == '\r') {
+            cr++;
+        }
+        else if (c == '\n') {
+            lf++;
+            if (lf == 2)
+            {
+                cr = 0;
+                lf = 0;
+                return result;
+            }
+        }
+        else {
+            result = realloc(result, ++size);
+            result[size-2] = c;
+            result[size-1] = '\0';
+        }
+    }
+    return NULL;
 }
 
 void reset_lcd(void)
@@ -43,9 +73,8 @@ void reset_lcd(void)
     if (TCNT1 >= 21600)
     {
         lcd_clear();
-        lcd_set_page(0);
-        lcd_set_column(10);
-        lcd_draw_char('S');
+        lcd_set_page(7);
+        lcd_set_column(5);
         // reset timer to prevent overflow
         TCNT1 = 0;
     }
--- experiments/uart/main.h
+++ experiments/uart/main.h
@@ -1,3 +1,4 @@
 #define UART_BAUD_RATE 19200
 
 void reset_lcd(void);
+char* uart_readline(void);
