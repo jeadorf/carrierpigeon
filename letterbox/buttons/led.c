97e635d4c85cb0af68d62fd0627e06806bcc5b5f
Marek Kubica <marek@xivilization.net>
2009-10-15 16:24:45 +0200

    Parametrized LED stuff

--- letterbox/buttons/led.c
+++ letterbox/buttons/led.c
@@ -1,33 +1,46 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
 #include "led.h"
 #include "global.h"
 
-void led_blink_times(int n)
+void led_blink_times(uint8_t led, int n)
 {
     int i;
     for (i = 0; i < n; i++)
     {
-        led_blink();
+        led_blink(led);
     }
 }
 
-void led_on(void)
+void led_init(void)
 {
+    // initialize green led
     set_bit(DDRC, DDC4);
-    set_bit(PORTC, PC4);
+    // initialize TODO led
+    set_bit(DDRC, DDC5);
+    // initialize TODO led
+    set_bit(DDRC, DDC6);
+
+    // initialize
+    set_bit(DDRB, DDB2);
+    set_bit(DDRB, DDB3);
 }
 
-void led_off(void)
+void led_on(uint8_t led)
 {
-    clear_bit(PORTC, PC4);
+    set_bit(PORTC, led);
 }
 
-
-void led_blink(void)
+void led_off(uint8_t led)
 {
-    led_on();
+    clear_bit(PORTC, led);
+}
+
+void led_blink(uint8_t led)
+{
+    led_on(led);
     _delay_ms(WAIT_TIME);
-    led_off();
+    led_off(led);
     _delay_ms(WAIT_TIME);
 }
--- letterbox/buttons/led.h
+++ letterbox/buttons/led.h
@@ -1,9 +1,11 @@
-#define LED_GREEN 0x10
-#define PORTC_MASK 0x10
+#define LED_GREEN PC4
+#define LED_RED PC5
+#define LED_BLUE PC6
+
 #define WAIT_TIME 150.0
 
-void led_blink(void);
-void led_on(void);
-void led_off(void);
-void led_blink_times(int n);
+void led_blink(uint8_t led);
+void led_on(uint8_t led);
+void led_off(uint8_t led);
+void led_blink_times(uint8_t led, int n);
 
